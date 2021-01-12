db51697c86612441fcfa6cf0e25617d1db6d491b
Matthew Nienow <rockopera8@gmail.com>
2021-01-11 21:14:34 -0600

    Added connect button, and signing functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/buttons.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,68 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React, {useState} from 'react';
 
 export default function Home() {
+  const [textValue, setTextValue] = useState('Test Data');
+  const [signature, setSignature] = useState('');
+  const [account, setAccount] = useState({});
+
+  function connect() {
+    if (WavesKeeper) {
+      WavesKeeper.publicState().then(account => {
+        console.log(account);
+        setAccount(account.account)
+      }).catch(error => {
+        alert('failed to connect to wallet');
+      });
+    } else {
+      alert('waves keeper not found');
+    }
+  }
+
+  function sign() {
+    WavesKeeper.auth({data: textValue}).then(auth => {
+      setSignature(auth.signature);
+    }).catch(error => {
+      alert('failed to sign');
+    });
+  }
+
+  function changeInput(event) {
+    setTextValue(event.target.value);
+  }
+
+  function renderContent() {
+    if (account.address) {
+      return (
+        <div>
+          <div className={styles.accountInfo}>
+            <div>{account.address}</div>
+            <div>{account.balance.available / 100000000} waves</div>
+            <div>{account.network} network</div>
+          </div>
+          <input type="text" value={textValue} onChange={changeInput}/>
+          <button className={styles.signButton} onClick={sign}>Sign Data</button>
+          <div className={styles.signatureOutput}>{signature}</div>
+        </div>
+      );
+    } else {
+      return <div>
+        <button className="flatButton" onClick={connect}>Connect Wallet</button>
+      </div>;
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Test Waves</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        {/*<button onClick={}>Unlock Wallet</button>*/}
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>Waves Test</h1>
+        {renderContent()}
       </main>
     </div>
   )
